2d732727d7fe10a620aba9d4665f0f0dfd680f17
Akshay K M <akshaykmfilms@gmail.com>
2024-11-11 19:10:40 +0530

    test

--- login.c
+++ login.c
@@ -1,6 +1,5 @@
 #include <stdlib.h>
 #include <stdio.h>
-<<<<<<< HEAD
 int main(void)
 {
     printf("hello");
@@ -9,9 +8,4 @@ int main(void)
     if (i==1){
     system("clear");    
     }
-=======
-
-int main(void){
-  system("clear");
->>>>>>> 63021ac065a2e91f24d05cb74fea6a08810b0f11
 }
