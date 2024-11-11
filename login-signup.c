47e51ff2e71f18809c07792b93291c2dc030130a
Invito <anuragraou@gmail.com>
2024-11-11 21:07:04 +0530

    Fixed stuff

--- login-signup.c
+++ login-signup.c
@@ -4,33 +4,32 @@
 #include "headers.h"
 
 void choices(void); // function to allow user to choose signup or login
-int main(void)
-{
+
+int main(void){
   clrscr();
-  printf("Welcome To Sigma Store\n");                                  
+  printf("Welcome To Sigma Store\n");
   printf("Enter 1 to signup\n");
   printf("Enter 2 to login\n");
   choices();
 }
 
-void choices(void)
-{
+void choices(void){
   int choice; // Chooses signup or login
   do{
-  scanf("%d",&choice);
-  if (choice==1)
-  {
-    clrscr();
-    printf("Welcome to signup");
-  }
-  else if (choice==2)
-  {
-    clrscr();
-    printf("Welcome to login");
-  }
-  else
-  {
-    printf("Invalid Input! Try again!\n");
-  }
+    scanf("%d",&choice);
+    if (choice==1)
+    {
+      clrscr();
+      printf("Welcome to signup");
+    }
+    else if (choice==2)
+    {
+      clrscr();
+      printf("Welcome to login");
+    }
+    else
+    {
+      printf("Invalid Input! Try again!\n");
+    }
   }while (choice != 1 && choice !=2);
 }
