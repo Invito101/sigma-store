02d4a23ac74ae8401a020070032d6fbd2aa8de6c
Invito <anuragraou@gmail.com>
2024-11-12 21:10:04 +0530

    Started on sqlite

--- database-control.c
+++ database-control.c
@@ -1,15 +1,15 @@
 #include <stdio.h>
 #include <sqlite3.h>
 
-int main(void) {
-    sqlite3 *db;           
-    int rc;                  
+int open_db(void) {
+    sqlite3 *db;
+    int rc;
 
     rc = sqlite3_open("SigmaStore.db", &db);
 
     if (rc != SQLITE_OK) {
         fprintf(stderr, "Cannot open database: %s\n", sqlite3_errmsg(db));
-        sqlite3_close(db);     // Close the database connection if it was partially opened
+        sqlite3_close(db); // Close the database connection if it was partially opened
         return 1;
     }
 
--- login-signup.c
+++ login-signup.c
@@ -6,35 +6,34 @@ void choices(void); // function to allow user to choose signup or login
 void signup(void);
 
 int main(void){
-  clrscr();
-  printf("Welcome To Sigma Store\n");
-  printf("Enter 1 to signup\n");
-  printf("Enter 2 to login\n");
-  choices();
+    clrscr();
+    printf("Welcome To Sigma Store\n");
+    printf("Enter 1 to signup\n");
+    printf("Enter 2 to login\n");
+    choices();
 }
 
 void choices(void){
-  int choice; // Chooses signup or login
-  do{
-    scanf("%d",&choice);
-    if (choice==1)
-    {
-      signup();
-    }
-    else if (choice==2)
-    {
-      clrscr();
-      printf("Welcome to login");
-    }
-    else
-    {
-      printf("Invalid Input! Try again!\n");
-    }
-  }while (choice != 1 && choice !=2);
+    int choice; // Chooses signup or login
+    do{
+        scanf("%d",&choice);
+        if (choice==1)
+        {
+            signup();
+        }
+        else if (choice==2)
+        {
+            clrscr();
+            printf("Welcome to login");
+        }
+        else
+        {
+            printf("Invalid Input! Try again!\n");
+        }
+    }while (choice != 1 && choice !=2);
 }
 
 void signup(void){ //Email password phonenumber name address state 
-  clrscr();
-  printf("Please enter your details below to signup");
-
+    clrscr();
+    printf("Please enter your details below to signup");
 }
